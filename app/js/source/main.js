a6dc5e81508a91f40a91630e94c82514a9fc5556
Nathan Hood <nathanhood@me.com>
2014-04-19 10:40:17 -0500

    2nd Move Debugged

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -6,13 +6,48 @@
 
   var selected;
   var moveTarget;
-
+  var continueTurn = false;
 
   function init(){
     boardCoordinates();
     initializeBoard();
-    $('#board').on('click', '.valid.current', select);
+    $('#board').on('click', '.valid.occupied.current', select);
     $('#board').on('click', '.valid:not(.occupied)', move);
+  }
+
+
+  function boardCoordinates(){
+    for(var j = 0; j < $('#board > tbody > tr').length; j++){
+      var row = $('#board > tbody > tr')[j];
+      var $row = $(row);
+      var tds = $row.children();
+
+      for(var i = 0; i < tds.length; i++){
+        var td = tds[i];
+        $(td).attr('data-x', i);
+        $(td).attr('data-y', j);
+      }
+    }
+  }
+
+  function initializeBoard(){
+    var td;
+    for(var i = 0; i < 24; i++){
+      td = $('#board > tbody > tr >td')[i];
+      if($(td).hasClass('checkers-black')){
+        var $img = $('<img>');
+        $(td).addClass('playerA occupied current').append($img);
+        $img.attr('src', './media/' + 'piece.png');
+      }
+    }
+    for(var j = 40; j < 64; j++){
+      td = $('#board > tbody > tr >td')[j];
+      if($(td).hasClass('checkers-black')){
+        var $img2 = $('<img>');
+        $(td).addClass('playerB occupied').append($img2);
+        $img2.attr('src', './media/' + 'kingA.gif');
+      }
+    }
   }
 
 
@@ -41,10 +76,13 @@
     if(Math.abs(vector[0]) + Math.abs(vector[1]) === 2){
       if(direction(selected, moveTarget)){
         movePiece();
+        continueTurn = false;
         endTurn();
+        endGame();
       }
-      debugger;
+
     } else if(Math.abs(vector[0]) + Math.abs(vector[1]) === 4){
+
       if(direction(selected, moveTarget)){
         var avgX = (finalX + initialX) / 2;
         var avgY = (finalY + initialY) / 2;
@@ -53,10 +91,20 @@
         if($deadPiece.hasClass('occupied') && !$deadPiece.hasClass('current')){
           $deadPiece.empty();
           $deadPiece.removeClass('occupied');
-          movePiece();
-          if(checkPotential() < 3){
-            endTurn();
+          if($deadPiece.hasClass('playerA')){
+            $deadPiece.removeClass('playerA');
+          } else {
+            $deadPiece.removeClass('playerB');
           }
+
+          movePiece();
+          if(checkPotential() < 4){
+            continueTurn = true;
+          } else {
+            continueTurn = false;
+          }
+          endTurn();
+          endGame();
         }
       }
     }
@@ -64,10 +112,13 @@
 
 
   function endTurn(){
-    var $currentPlayer = $('td.current');
-    var $newPlayer = $('td.occupied');
-    $newPlayer.addClass('current');
-    $currentPlayer.removeClass('current');
+    if(!continueTurn){
+      var $currentPlayer = $('td.current');
+      var $newPlayer = $('td.occupied');
+
+      $newPlayer.addClass('current');
+      $currentPlayer.removeClass('current');
+    }
   }
 
   function movePiece(){
@@ -130,13 +181,18 @@
 
   }
 
+  function checkDead(deadPiece){
+    return deadPiece.hasClass('occupied') && !deadPiece.hasClass('current');
+  }
+
   function checkPotential(){
+
     var potentialX = [];
     var potentialY = [];
     var potentialTargets = [];
 
-    //generates x,y values of four possible moves for sequential turns
-    for(var i = -2; i < 3; i + 4){
+    // generates x,y values of four possible moves for sequential turns
+    for(var i = -2; i <= 2; i += 4){
       potentialX.push(moveTarget.data('x') + i);
       potentialY.push(moveTarget.data('y') + i);
     }
@@ -149,56 +205,27 @@
     }
 
     //filter possible moves
-    debugger;
     var spliceTargets = [];
-    for(var l = 0; l < 4; l++){
+    for(var l = 0; l < potentialTargets.length; l++){
       var $dead = generateDead(moveTarget, potentialTargets[l]);
-      if(potentialTargets[l].hasClass('occupied') || !direction(moveTarget, potentialTargets[l])){
-        if($dead.hasClass('current') || !$dead.hasClass('occupied')){
+      if(potentialTargets[l].hasClass('occupied') || !checkDead($dead) || !direction(moveTarget, potentialTargets[l])){
           spliceTargets.push(l);
-        }
+        // }
       }
     }
 
-    console.log(spliceTargets.length);
     return spliceTargets.length;
   }
 
-
-  function boardCoordinates(){
-    for(var j = 0; j < $('#board > tbody > tr').length; j++){
-      var row = $('#board > tbody > tr')[j];
-      var $row = $(row);
-      var tds = $row.children();
-
-      for(var i = 0; i < tds.length; i++){
-        var td = tds[i];
-        $(td).attr('data-x', i);
-        $(td).attr('data-y', j);
+  function endGame(){
+    if($('td.current').length === 0){
+      if($('td.playerA').length === 0){
+        alert('The Beatles Win!');
+      } else {
+        alert('The Rolling Stones Win!');
       }
     }
   }
-
-  function initializeBoard(){
-    var td;
-    for(var i = 0; i < 24; i++){
-      td = $('#board > tbody > tr >td')[i];
-      if($(td).hasClass('checkers-black')){
-        var $img = $('<img>');
-        $(td).addClass('playerA occupied current').append($img);
-        $img.attr('src', './media/' + 'piece.png');
-      }
-    }
-    for(var j = 40; j < 64; j++){
-      td = $('#board > tbody > tr >td')[j];
-      if($(td).hasClass('checkers-black')){
-        var $img2 = $('<img>');
-        $(td).addClass('playerB occupied').append($img2);
-        $img2.attr('src', './media/' + 'kingA.gif');
-      }
-    }
-  }
-
 
 
 })();
